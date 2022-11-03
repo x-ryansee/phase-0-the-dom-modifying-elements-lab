6a54c87778941f64c3dfa32533d24e77eef8649a
Ryan See <ryanjosephsee@gmail.com>
2022-11-03 10:21:39 -0700

    commit tree 21dfa6c9f2a6f34a474b5add8c886adb752940b3

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.querySelector("#main");
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+
+newHeader.textContent = "Ryan is the champion";
+
